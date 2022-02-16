f3b0df5b9e6e2ee25de2ed6833a4f184c35e5022
Nick Monsma <nmonsma@outlook.com>
2022-02-16 12:21:21 -0500

    feat: update

--- script/presentation.js
+++ script/presentation.js
@@ -1,35 +1,24 @@
 // Global Variables
-let fileList = [""]
-let team2 = 0;
-let lastAdd = 0;
+let imageList = ['', 'img/Europe.png', 'img/WWI.png']
+let imageIndex = 0
 
 // Main Functions
-function addTeam1 () {
-    team1++;
-    console.log("adding to team 1");
-    document.getElementById('score-1').innerText = team1;
-    lastAdd = 1;
+function advanceImage () {
+    imageIndex++;
+    console.log(`${imageList[imageIndex]}`);
+    document.getElementById('body').style.backgroundImage=`${imageList[imageIndex]}`;
 }
 
-function addTeam2 () {
-    team2++;
-    console.log("adding to team 2");
-    document.getElementById('score-2').innerText = team2;
-    lastAdd = 2;
-}
-
-function undo () {
-    if (lastAdd==1) {team1--};
-    if (lastAdd==2) {team2--};
-    document.getElementById('score-1').innerText = team1;
-    document.getElementById('score-2').innerText = team2;
+function reverseImage () {
+    imageIndex--;
+    console.log(`${imageList[imageIndex]}`);
+    document.getElementById('body').style.backgroundImage=`${imageList[imageIndex]}`;
 }
 
 // Main Actions
 document.addEventListener('keydown', (e)=> {
     const keyName = e.key;
     console.log(keyName);
-    if (keyName==="1") addTeam1();
-    if (keyName==="2") addTeam2();
-    if (keyName==="0") undo();
+    if (keyName==="1") advanceImage();
+    if (keyName==="0") reverseImage();
 })
