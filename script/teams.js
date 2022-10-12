433454d3ce66f073b1197d2caeffd6f826c16713
Nick Monsma <nmonsma@gmail.com>
2022-10-12 09:22:07 -0400

    added a fourth team

--- script/teams.js
+++ script/teams.js
@@ -2,6 +2,7 @@
 let team1 = 0;
 let team2 = 0;
 let team3 = 0;
+let team4 = 0;
 let lastAdd = 0;
 
 // Main Functions
@@ -26,13 +27,23 @@ function addTeam3 () {
     lastAdd = 3;
 }
 
+function addTeam4 () {
+    team4++;
+    console.log("adding to team 4");
+    document.getElementById('score-4').innerText = team4;
+    lastAdd = 4;
+}
+
 function undo () {
     if (lastAdd==1) {team1--};
     if (lastAdd==2) {team2--};
     if (lastAdd==3) {team3--};
+    if (lastAdd==4) {team4--};
+
     document.getElementById('score-1').innerText = team1;
     document.getElementById('score-2').innerText = team2;
     document.getElementById('score-3').innerText = team3;
+    document.getElementById('score-4').innerText = team4;
 }
 
 // Main Actions
@@ -42,5 +53,6 @@ document.addEventListener('keydown', (e)=> {
     if (keyName==="1") addTeam1();
     if (keyName==="2") addTeam2();
     if (keyName==="3") addTeam3();
+    if (keyName==="4") addTeam4();
     if (keyName==="0") undo();
 })
