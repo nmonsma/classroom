7781b2eeb0243f1e37e1a377390d56c27b121579
Nick Monsma <nmonsma@gmail.com>
2022-12-13 11:09:19 -0500

    Categorial Syllogism updates

--- script/catsyl.js
+++ script/catsyl.js
@@ -1,58 +1,34 @@
 // Variables
-const calcSpace = document.getElementById('calc-space');
-const orderSpace = document.getElementById('order-space');
-
+const terms = document.getElementsByClassName('term')
+const figure1 = document.getElementsByClassName('figure-1');
+const figure2 = document.getElementsByClassName('figure-2');
+const figure3 = document.getElementsByClassName('figure-3');
+const figure4 = document.getElementsByClassName('figure-4');
+const figureSet = [figure1, figure2, figure3, figure4];
+const majors = document.getElementsByClassName('major');
+const minors = document.getElementsByClassName('minor');
 
 // Helper Functions
-function createOrderQuestion() {
-    const createdDiv = document.createElement('div');
-    createdDiv.classList.add('order-question');
-    createdDiv.classList.add('order');
+function highlightTerms(figure) {
+    if (figure=="`") {
+        for (i=0; i<terms.length; i++) {terms[i].classList.remove('highlight')}
+    } else {
+        for (i=0; i<figureSet[figure-1].length; i++) {figureSet[figure-1][i].classList.add('highlight')}
+    }
+}
 
-    const questionNumber = Math.floor(Math.random()*key.order.length);
-    const stepNumber = Math.floor(Math.random()*(key.order[questionNumber].solution.length));
-    
-    const problemParagraph = document.createElement('p');
-    const problemText = key.order[questionNumber].problem;
-    problemParagraph.classList.add('order-problem');
-    problemParagraph.innerHTML = `${problemText}<br>If assigning truth values to the propositions is step 0, what is step ${stepNumber+1}?`;
-    
-    const solutionParagraph = document.createElement('p');
-    const solutionText = key.order[questionNumber].problem.charAt(key.order[questionNumber].solution[stepNumber]);
-    solutionParagraph.classList.add('solution');
-    solutionParagraph.classList.add('invisible');
-    solutionParagraph.innerText = `${solutionText}`;
-
-    createdDiv.appendChild(problemParagraph);
-    createdDiv.appendChild(solutionParagraph);
-
-    return createdDiv;
+function colorMajorMinor() {
+    for (i=0; i<majors.length; i++) {majors[i].classList.toggle('red')};
+    for (i=0; i<minors.length; i++) {minors[i].classList.toggle('blue')};
 }
 
 // Main Functions
-
-function toggleSolutions() {
-    const solutionsObjects = document.getElementsByClassName('solution');
-    for (i=0;i<solutionsObjects.length;i++) {
-        solutionsObjects[i].classList.toggle('invisible');
-    }
- }
-
-// Main Actions
-for (i=0;i<7;i++) {
-    const childElement = createCalculationQuestion();
-    calcSpace.appendChild(childElement);
-}
-for (i=0;i<13;i++) {
-    const childElement = createResolutionQuestion();
-    calcSpace.appendChild(childElement);
-}
-const titleSpace = document.createElement('p');
-titleSpace.innerHTML = `Name: ________________<br><br>Date: __________`;
-orderSpace.appendChild(titleSpace);
-for (i=0;i<6;i++) {
-    const childElement = createOrderQuestion();
-    orderSpace.appendChild(childElement);
-}
-
-document.addEventListener('click', toggleSolutions);
+document.addEventListener('keydown', (e)=> {
+    const keyName = e.key;
+    console.log(keyName);
+    if (keyName==="Enter") {
+        colorMajorMinor()
+    } else {
+        highlightTerms(keyName)
+    };
+});
