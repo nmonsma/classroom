e7a05a6fcd126b0d95198b98faadf70bb8887528
Nick Monsma <nmonsma@gmail.com>
2023-02-06 09:57:15 -0500

    syllogism updates

--- script/immediate.js
+++ script/immediate.js
@@ -1,13 +1,4 @@
 // Variables
-const terms = document.getElementsByClassName('term')
-const figure1 = document.getElementsByClassName('figure-1');
-const figure2 = document.getElementsByClassName('figure-2');
-const figure3 = document.getElementsByClassName('figure-3');
-const figure4 = document.getElementsByClassName('figure-4');
-const figureSet = [figure1, figure2, figure3, figure4];
-const majors = document.getElementsByClassName('major');
-const minors = document.getElementsByClassName('minor');
-
 let statement1 = {
     "affirmative": true,
     "universal": true,
@@ -70,20 +61,7 @@ function contrapositive(statement){
     }    
 }
 
-function highlightTerms(figure) {
-    colorMajorMinor();
-    if (figure==0) {for (i=0; i<terms.length; i++) {terms[i].classList.remove('highlight')}}
-    else {for (i=0; i<figureSet[figure-1].length; i++) {figureSet[figure-1][i].classList.add('highlight')}}   
-}
-
-function colorMajorMinor() {
-    for (i=0; i<majors.length; i++) {majors[i].classList.toggle('red')};
-    for (i=0; i<minors.length; i++) {minors[i].classList.toggle('blue')};
-}
-
 // Main Functions
-document.addEventListener('keydown', (e)=> {
-    const keyName = e.key;
-    console.log(keyName);
-    if (figure>-1&&figure<5) {highlightTerms(keyName)};
-});
+function createStatement (){
+    
+}
