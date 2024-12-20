9a6120cf29273ff4ef99f734727f781640e8e89e
Nick Monsma <nmonsma@gmail.com>
2024-12-20 07:45:00 -0500

    jeopardy--to allow more scores elements and Control+click to reduce score

--- script/jeopardy.js
+++ script/jeopardy.js
@@ -1,7 +1,8 @@
 // Global Variables
-let scores = [0,0,0,0,0];
+let scores = [];
 let answerDisplayed = '';
 let valueDisplayed = 0;
+let shiftDepressed = false;
 const items = document.getElementsByClassName('item');
 const scoresElements = document.getElementsByClassName('scores');
 
@@ -21,9 +22,13 @@ function decreaseScores(i) {
 
 function processClick() {
     // console.log(this);
-    console.log(this.id.substr(5,6));
-    processChoice(this.id.substr(5,6));
-}
+    if (shiftDepressed) {
+        processChoice(parseInt(this.id.substr(5,6),10)+99); //If the control key is depressed, send the point value + 99 to indicate a reduction in points 
+    } 
+    else {
+        processChoice(this.id.substr(5,6)) //Send the point value (from the element id.subst(5,6) to the processChoice function)
+    }
+};
 
 // Main Functions
 function processChoice(choiceCode) {
@@ -38,13 +43,21 @@ function processChoice(choiceCode) {
         answerDisplayed = '';
         valueDisplayed = 0;
     }
-    else if ((choiceCode>0)&&(choiceCode<6)) {
+    else if ((choiceCode>0)&&(choiceCode<100)) {
         scores[choiceCode-1]=scores[choiceCode-1]+valueDisplayed;
         increaseScores(choiceCode-1);
     }
-    else if ((choiceCode>5)&&(choiceCode<11)) {
-        scores[choiceCode-6]=scores[choiceCode-6]-valueDisplayed;
-        decreaseScores(choiceCode-6);
+    
+    //When using number keys to remove points, remove those points:
+    // else if ((choiceCode>5)&&(choiceCode<11)) {
+    //     scores[choiceCode-6]=scores[choiceCode-6]-valueDisplayed;
+    //     decreaseScores(choiceCode-6);
+    // }
+
+    //When using a 100-code to remove points, remove those points:
+    else if ((choiceCode>99)&&(choiceCode<200)) {
+        scores[choiceCode-100]=scores[choiceCode-100]-valueDisplayed;
+        decreaseScores(choiceCode-100);
     }
     else if (answerDisplayed=='') {
         document.getElementById(`question-${choiceCode}`).classList.add('front');
@@ -55,10 +68,17 @@ function processChoice(choiceCode) {
 }
 
 // Main Actions
+
 document.addEventListener('keydown', (e)=> {
     let keyPressed = e.key;
+    console.log(keyPressed);
     if (keyPressed==0) {keyPressed=10};
-    processChoice(keyPressed);
+    if (keyPressed=="Control") {shiftDepressed=true} else {processChoice(keyPressed)};
+});
+
+document.addEventListener('keyup', (e)=> {
+    let keyReleased = e.key;
+    if (keyReleased=="Control") {shiftDepressed=false};
 });
 
 for (i=0;i<items.length;i++) {
@@ -66,5 +86,6 @@ for (i=0;i<items.length;i++) {
 }
 
 for (i=0;i<scoresElements.length;i++) {
+    scores.push(0); //Add a list item for each score element in the html.
     scoresElements[i].addEventListener('click',processClick);
 }
